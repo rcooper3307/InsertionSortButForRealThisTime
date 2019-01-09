73b27229e2fca9542946dd46393db06ef82350b3
BT_1N3_21@bktech.local <BT_1N3_21@bktech.local>
2019-01-09 08:40:16 -0500

    Initial commit

---
+++ src/com/company/InsertionSort.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class InsertionSort {
+    public static void insertionSort(int[] arr)
+    {
+        //once you have the array, you look at index 1 and the rest and if its less than index 0 you switch the numbers
+        //you take out the number and put it in a temp, then you move every value in the index+1, and then drop the temp
+        //in the empty space where it should be
+        //if youve got like 6 slots open, and you open the 7th, and its less than the 5th but greater than the 4th, you
+        //dont move anything before the 4th but from then on, you put the 7th in a temp, and then move 6 and 5 forward
+        //and then put the 7th in the empty spot
+        //
+    }
+
+}
